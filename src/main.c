e71871baea730cbd25c2710738e79c7ee6e4385a
oussama458 <63619120+oussama458@users.noreply.github.com>
2023-06-03 15:49:04 +0100

    added gsm function

--- src/main.c
+++ src/main.c
@@ -59,6 +59,47 @@ void fermeture(){
     delay(20);
   }
 
+void compactage () {
+    Serial.printIn("Compactage....");
+    digitalWrite(sens1,HIGH);
+    delay(2000);
+    Serial.printIn("Decompactage....");
+    digitalWrite(sens1,LOW);
+    digitalWrite(sens2,HIGH);
+    delay(2000);
+    digitalWrite(sens1,LOW);
+};
+
+/*
+        LES FONCTIONS DE GSM :
+*/
+
+
+void receive_message()
+{
+  if (SIM900.available() > 0)
+  {
+    incomingData = SIM900.readString(); // Get the data from the serial port.
+    Serial.print(incomingData); 
+    delay(10); 
+  }
+}
+
+void send_message(String message)
+{
+  SIM900.println("AT+CMGF=1");    //Set the GSM Module in Text Mode
+  delay(100);  
+  SIM900.println("AT+CMGS=\"+971xxxxxxxx\""); // Replace it with your mobile number
+  delay(100);
+  SIM900.println(message);   // The SMS text you want to send
+  delay(100);
+  SIM900.println((char)26);  // ASCII code of CTRL+Z
+  delay(100);
+  SIM900.println();
+  delay(1000);  
+}
+ 
+
 
 
 
